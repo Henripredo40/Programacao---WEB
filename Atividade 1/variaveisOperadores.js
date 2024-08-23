d9e75ae8ebe9396b43a582441f62030d1a766238
PEDRO HENRIQUE NUNES DE FREITAS <pedro.hfreitas@a.ucb.br>
2024-08-23 10:13:31 -0300

    envio atividade 1

---
+++ Atividade 1/variaveisOperadores.js
@@ -0,0 +1,15 @@
+let a = 5;
+let b = 3;
+let c;
+
+c = a + b;
+console.log('A Soma entre ${a} e ${b} é ${c}');
+
+c = a - b;
+console.log('A subtraçao entre ${a} e ${b} é ${c}'); 
+
+c = a * b;
+console.log('A multiplicaçao entre ${a} e ${b} é ${c}');
+
+c = a / b ;
+console.log('A divisao entre ${a} e ${b} é ${c}');
